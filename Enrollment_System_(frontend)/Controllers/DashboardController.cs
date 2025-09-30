ab42c5e4b41c85e54180c0a82df887ca942daf4f
khel <obidos.michael.cautiber@gmail.com>
2025-09-30 21:29:20 +0800

    added dashboard page

---
+++ Enrollment_System_(frontend)/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Enrollment_System__frontend_.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult DashboardPage()
+        {
+            return View();
+        }
+    }
+}
